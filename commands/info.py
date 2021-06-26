0637e9c97d2408f0f6fedd3f6bff29e994a0f5b1
DML <81225500+Fuhjscj@users.noreply.github.com>
2021-06-26 17:47:15 +0300

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -19,37 +19,31 @@ async def info_wrapper(message: Message, **kwargs):
     db = Database.get_current()
     version_rest = requests.get(const.VERSION_REST).json()
 
-    if version_rest['version'] != const.__version__:
-        update_text = f"\n\n⚠ Доступно обновление {version_rest['version']}\n" \
+   if version_rest['version'] != const.__version__:
+        update_text = f"\n\n⚠🚫Обновить надо🥵 {version_rest['version']}\n" \
                       f"{version_rest['description']}\n"
     else:
         update_text = ""
 
     text = f"""
-    ❤ IDM LP v{__version__} by {__author__}
-
-    ▶ Ключ рукаптчи: {"&#9989;" if db.ru_captcha_key else "&#10060;"}
-    ▶ Удаление уведомлений: {"&#9989;" if db.delete_all_notify else "&#10060;"}
-    ▶ Выключение уведомлений: {"&#9989;" if db.disable_notifications else "&#10060;"}
-
-    ▶ В игноре: {len(db.ignored_members)}
-    ▶ В глобальном игноре: {len(db.ignored_global_members)}
-    ▶ В муте: {len(db.muted_members)}
-    ▶ Довов: {len(db.trusted)}
-    ▶ Алиасов: {len(db.aliases)}
-    ▶ Шаблонов для удаления: {len(db.regex_deleter)}
-
-    ▶ Выходить из бесед: {"&#9989;" if db.auto_exit_from_chat else "&#10060;"}
-    ▶ Удалять диалог: {"&#9989;" if db.auto_exit_from_chat_delete_chat else "&#10060;"}
-    ▶ Добавлять пригласившего в ЧС: {"&#9989;" if db.auto_exit_from_chat_add_to_black_list else "&#10060;"}
-    
-    ▶ Повторялка: {"&#9989;" if db.repeater_active else "&#10060;"}
-    ▶ Триггер на повторялку: {db.repeater_word}
-        
-    ▶ Префикс ДД: {db.dd_prefix}
-    ▶ Сервисные префиксы: {' '.join(db.service_prefixes)}
-    ▶ Свои префиксы: {' '.join(db.self_prefixes) if db.self_prefixes else ''}
-    ▶ Префиксы дежурного: {' '.join(db.duty_prefixes) if db.duty_prefixes else ''}{update_text}
+    🤑☺️ DМL LP ❤️ v{__version__}
+    |🔴 Ключ рукаптчи: {"✅" if db.ru_captcha_key else "❌"}
+    |🟠 Удаление уведомлений: {"✅" if db.delete_all_notify else "❌"}
+    |🟡 Выключение уведомлений: {"✅" if db.disable_notifications else "❌"}
+    |🟢 В игноре: {len(db.ignored_members)}
+    |🔵 В глобальном игноре: {len(db.ignored_global_members)}
+    |🟣 В муте: {len(db.muted_members)}
+    |🟤 Довов: {len(db.trusted)}
+    |⚫ Алиасов: {len(db.aliases)}
+    |⚪ Шаблонов для удаления: {len(db.regex_deleter)}
+    |🔴 Выходить из бесед: {"✅" if db.auto_exit_from_chat else "❌"}
+    |🟠 Удалять диалог: {"✅" if db.auto_exit_from_chat_delete_chat else "❌"}
+    |🟡 Добавлять пригласившего в ЧС: {"✅" if db.auto_exit_from_chat_add_to_black_list else "❌"}
+    |🟢 Повторялка: {"✅" if db.repeater_active else "❌"}
+    |🔵 Триггер на повторялку: {db.repeater_word}       
+    |🟣 Сервисные префиксы: {' '.join(db.service_prefixes)}
+    |🟤 Свои префиксы: {' '.join(db.self_prefixes) if db.self_prefixes else ''}
+    |⚫ Префиксы дежурного: {' '.join(db.duty_prefixes) if db.duty_prefixes else ''}{update_text}
     """.replace('    ', '')
     await edit_message(
         message,
